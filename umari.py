17949aab131d8c3f12cac1cbeaeb281c645602ff
Umaritfs <63997482+Umaritfs@users.noreply.github.com>
2020-04-27 11:10:33 +0500

    Update and rename zeroboy.py to umari.py

--- umari.py
+++ umari.py
@@ -49,17 +49,17 @@ def jalan(z):
 ##### LOGO #####
 logo = """ -----------------------------•◈•
 (  __)\\ ____--------------_------------•◈•
-|__(~)    •||•THE - AAHIL -OFFICAL------•◈•
-|__\~~) •||•RANA - RAJPUT---------------•◈•
-|__(-----\  •◈•------BLACK-TIGER--------•◈•
+|__(~)    •||•THE - UMARI -TFS------•◈•
+|__\~~) •||•UMARI - Tfs---------------•◈•
+|__(-----\  •◈•------UMARI TFS---------•◈•
 |__~~~\ •◈•-----█-------⑦-------█------•◈•
 |__~~~\ •◈•-----█-------⑧-------█------•◈•
 |__~~~\ •◈•-----█-------⑥-------█------•◈•
 \033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;FlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1; Aahilrna4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mUmari
+\033[1;96mInstagram \033[1;93m: \033[1;FlowUmari
+\033[1;96mFacebook  \033[1;93m: \033[1; Umaritfs346
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/umari/tfs
 \033[1;91m======================================="""
 
 def tik():
@@ -80,15 +80,15 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print "\033[1;96m ============================================================="
 print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mUmari Tfs
+\033[1;96mInstagram \033[1;93m: \033[1;92mFlowUmari
+\033[1;96mFacebook  \033[1;93m: \033[1;92m Umati346
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Umari/tfs
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "rana"
-CorrectPassword = "rana"
+CorrectUsername = "Umari"
+CorrectPassword = "Tfs"
 
 loop = 'true'
 while (loop == 'true'):
